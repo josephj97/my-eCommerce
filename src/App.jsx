b1d1630b8b4388ea32ca666f4e23df3e9af7d85f
Joseph Atef <josephatef.j97@gmail.com>
2024-11-11 02:07:39 +0200

    feat: set up BrowserRouter in App.jsx for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
