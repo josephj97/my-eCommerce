b5b61bfdd3d443f58359e64beb1a3601d99f3af1
Joseph Atef <josephatef.j97@gmail.com>
2025-05-10 00:05:11 +0300

    feat: add search bar to navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -107,17 +107,50 @@ const Navbar = () => {
                   Today's Deals
                 </a>
               </li>
-              <li className="shrink-0">
-                <a
-                  href="#"
-                  title=""
-                  className="text-sm font-medium text-gray-900 hover:text-primary-700 dark:text-white dark:hover:text-primary-500"
-                >
-                  Sell
-                </a>
-              </li>
             </ul>
           </div>
+
+          {/* Search bar */}
+          <form class="w-full max-w-2xl mx-auto">
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Phones, Laptops..."
+                required
+              />
+              <button
+                type="submit"
+                class="text-white absolute end-2.5 bottom-2.5 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
 
           <div className="flex items-center lg:space-x-2">
             <div className="flex-col">
@@ -425,7 +458,7 @@ const Navbar = () => {
                   role="button"
                 >
                   {' '}
-                  Proceed to Checkout{' '}
+                  Go to my checkout{' '}
                 </a>
               </div>
             </div>
@@ -498,46 +531,6 @@ const Navbar = () => {
                     >
                       {' '}
                       My Orders{' '}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      title=""
-                      className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      {' '}
-                      Settings{' '}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      title=""
-                      className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      {' '}
-                      Favourites{' '}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      title=""
-                      className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      {' '}
-                      Delivery Addresses{' '}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      title=""
-                      className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      {' '}
-                      Billing Data{' '}
                     </a>
                   </li>
                 </ul>
@@ -614,30 +607,6 @@ const Navbar = () => {
                 className="hover:text-primary-700 dark:hover:text-primary-500"
               >
                 Gift Ideas
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="hover:text-primary-700 dark:hover:text-primary-500"
-              >
-                Games
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="hover:text-primary-700 dark:hover:text-primary-500"
-              >
-                Electronics
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="hover:text-primary-700 dark:hover:text-primary-500"
-              >
-                Home & Garden
               </a>
             </li>
           </ul>
