78c878f66c7469a694e1094ed55efe3ba7d64cb5
Joseph Atef <josephatef.j97@gmail.com>
2025-05-17 01:30:43 +0300

    feat(context): add functions and local storage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,12 +1,26 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  // const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (item) => {
-    setCartItems((prevCartItem) => [...prevCartItem, item]);
+    setCartItems((prevCartItem = []) => {
+      const exists = prevCartItem.find((cartItem) => cartItem.id === item.id);
+      if (exists) {
+        return prevCartItem;
+      }
+      return [...prevCartItem, { ...item, quantity: 1 }];
+    });
   };
 
   const removeFromCart = (itemId) => {
@@ -15,10 +29,19 @@ const CartProvider = ({ children }) => {
     );
   };
 
+  const updateQuantity = (itemId, newQuantity) => {
+    setCartItems((prevCartItem) =>
+      prevCartItem.map((item) =>
+        item.id === itemId ? { ...item, quantity: newQuantity } : item
+      )
+    );
+  };
+
   const contextValue = {
     cartItems,
     addToCart,
     removeFromCart,
+    updateQuantity,
   };
   return (
     <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
