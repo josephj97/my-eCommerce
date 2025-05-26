3953ccf93dfde46d521bc38d74e66eeba4bf7195
Joseph Atef <josephatef.j97@gmail.com>
2025-05-26 23:29:10 +0300

    feat: Integrate auth routes and implement ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Cart from './components/Cart/Cart';
-import Categories from './components/Categories/Categories';
-import Products from './components/Products/Products';
-import ProductDetail from './components/Products/ProductDetail';
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Cart from "./components/Cart/Cart";
+import Categories from "./components/Categories/Categories";
+import Products from "./components/Products/Products";
+import ProductDetail from "./components/Products/ProductDetail";
+import SignupPage from "./components/Auth/SignUpPage";
+import LoginPage from "./components/Auth/LoginPage";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
 
 function App() {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/login", "/signup"];
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-        </Routes>
-      </BrowserRouter>
+      {!shouldHideNavbar && <Navbar />}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   );
 }
