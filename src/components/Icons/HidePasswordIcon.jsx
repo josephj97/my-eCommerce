a239cb16549f5f7e826ffce4c06afcef810f26f5
Joseph Atef <josephatef.j97@gmail.com>
2025-05-28 21:50:56 +0300

    feat(ui): add custom ShowPassword Icon and HidePassword Icon components

---
+++ src/components/Icons/HidePasswordIcon.jsx
@@ -0,0 +1,19 @@
+const HidePasswordIcon = () => (
+  <svg
+    className="w-5 h-5 text-gray-400"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.98 8.223a10.477 10.477 0 00-.955 1.252C2.322 10.203 2 11 2 12s.322 1.797 1.025 2.525C4.856 16.953 7.886 18.75 12 18.75c1.384 0 2.65-.222 3.777-.637m3.041-2.24a10.43 10.43 0 002.202-3.223c.293-.69.48-1.43.48-2.15s-.187-1.46-.48-2.15a10.477 10.477 0 00-1.28-2.014M9.53 9.53a3.75 3.75 0 005.94 4.22M14.25 14.25L5.25 5.25"
+    />
+  </svg>
+);
+
+export default HidePasswordIcon;
